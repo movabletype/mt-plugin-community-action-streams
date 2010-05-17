87587a2e68bc745b46348c271ca6802a975a6dc9
asawada <asawada@f3b680aa-fef1-0310-ac4d-b15a123b9462>
2010-05-17 05:54:52 +0000

    Revised ja.pm
    git-svn-id: http://svn.sixapart.com/repos/eng/mtplugins/trunk/trunk/plugins/CommunityActionStreams@124098 f3b680aa-fef1-0310-ac4d-b15a123b9462

--- lib/CommunityActionStreams/L10N/ja.pm
+++ lib/CommunityActionStreams/L10N/ja.pm
@@ -11,9 +11,16 @@ use base 'CommunityActionStreams::L10N::en_us';
 use vars qw( %Lexicon );
 %Lexicon = (
 
-## plugins/CommunityActionStreams/config.yaml
-	'Action streams for community events: add entry, add comment, add favorite, follow user.' => 'エントリー、コメント、お気に入り、ユーザーの注目などのコミュニティイベント用アクションストリームです。',
-
+## config.yaml
+ 'Action streams for community events: add entry, add comment, add favorite, follow user.' => 'エントリー、コメント、お気に入り、ユーザーの注目などのコミュニティイベント用アクションストリームです。',
+ 'Community Action Streams' => 'Community Action Streams',
+ 'Favorites' => 'お気に入り',
+ 'Followers' => 'フォロワー',
+ 'Replies' => '返信',
+ '[_1] <a href="[_2]">commented</a> on <a href="[_3]">[_4]</a>' => '[_1] は<a href="[_3]">[_4]</a>に<a href="[_2]">コメント</a>しました',
+ '[_1] is now following <a href="[_2]">[_3]</a>' => '[_1]は<a href="[_2]">[_3]</a>をフォローしました',
+ '[_1] posted <a href="[_2]">[_3]</a>' => '[_1]は<a href="[_2]">[_3]</a>を投稿しました',
+ '[_1] saved <a href="[_2]">[_3]</a> as a favorite' => '[_1]は<a href="[_2]">[_3]</a>をお気に入りに保存しました',
 );
 
 1;
