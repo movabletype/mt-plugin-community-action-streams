ef31b6105c3d69d9f02472a46e6ad485a5052d62
asawada <asawada@f3b680aa-fef1-0310-ac4d-b15a123b9462>
2010-05-18 08:34:43 +0000

    Revised ja.pm. case #104025.
    git-svn-id: http://svn.sixapart.com/repos/eng/mtplugins/trunk/trunk/plugins/CommunityActionStreams@124114 f3b680aa-fef1-0310-ac4d-b15a123b9462

--- lib/CommunityActionStreams/L10N/ja.pm
+++ lib/CommunityActionStreams/L10N/ja.pm
@@ -18,7 +18,7 @@ use vars qw( %Lexicon );
  'Followers' => 'フォロワー',
  'Replies' => '返信',
  '[_1] <a href="[_2]">commented</a> on <a href="[_3]">[_4]</a>' => '[_1] は<a href="[_3]">[_4]</a>に<a href="[_2]">コメント</a>しました',
- '[_1] is now following <a href="[_2]">[_3]</a>' => '[_1]は<a href="[_2]">[_3]</a>をフォローしました',
+ '[_1] is now following <a href="[_2]">[_3]</a>' => '[_1]は<a href="[_2]">[_3]</a>を注目しました',
  '[_1] posted <a href="[_2]">[_3]</a>' => '[_1]は<a href="[_2]">[_3]</a>を投稿しました',
  '[_1] saved <a href="[_2]">[_3]</a> as a favorite' => '[_1]は<a href="[_2]">[_3]</a>をお気に入りに保存しました',
 );
