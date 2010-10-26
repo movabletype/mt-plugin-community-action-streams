b7ee4792c2ea4d596b2cea7298fd4a7f68d6ca3a
Finn Smith <finn@timeghost.net>
2010-10-26 13:28:52 -0400

    Update license, minor formatting fixes.

--- plugins/CommunityActionStreams/lib/CommunityActionStreams/L10N/de.pm
+++ plugins/CommunityActionStreams/lib/CommunityActionStreams/L10N/de.pm
@@ -1,18 +1,25 @@
-# Community Action Streams (C) 2009-2010 Six Apart, Ltd. All Rights Reserved.
-# This library is free software; you can redistribute it and/or modify it
-# under the same terms as Perl itself.
-#
-# $Id$
+############################################################################
+# Copyright © 2010 Six Apart Ltd.
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# version 2 for more details. You should have received a copy of the GNU
+# General Public License version 2 along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
 
 package CommunityActionStreams::L10N::de;
 
 use strict;
+
 use base 'CommunityActionStreams::L10N::en_us';
 use vars qw( %Lexicon );
 %Lexicon = (
 
 ## plugins/CommunityActionStreams/config.yaml
-	'Action streams for community events: add entry, add comment, add favorite, follow user.' => 'Action Streams für Community-Ereignisse: Eintrag hinzufügen, Kommentar hinzufügen, Favoriten hinzufügen, Benutzer folgen',
+    'Action streams for community events: add entry, add comment, add favorite, follow user.' => 'Action Streams für Community-Ereignisse: Eintrag hinzufügen, Kommentar hinzufügen, Favoriten hinzufügen, Benutzer folgen',
 
 
 );
